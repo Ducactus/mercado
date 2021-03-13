5f894749ed45a345a69605e0fd3e975a86c0caa3
DashCell <lucas_vinicius10@live.com>
2021-03-13 18:48:11 -0300

    feat(Model): Implementa validações nos models
     Sem mudanças nas migrations.

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,4 +1,10 @@
 class Cliente < ApplicationRecord
 	has_many :clientes_produtos
 	has_many :produtos, through: :clientes_produtos
+
+	validates :nome,:telefone,:endereco, presence: { message: " Campo deve ser preenchido."}
+	validates :nome, uniqueness: { message: " Já cadastrado."}
+	validates :telefone, uniqueness: { message: " Já cadastrado."} , on: :update
+
+
 end
--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,12 @@
 class Produto < ApplicationRecord
 	has_many :clientes_produtos
 	has_many :clientes, through: :clientes_produtos
+
+	validates :nome,:qtd,:valor, presence: {message: " campo não preenchido."}
+	validates :descricao, length: {maximum: 250}
+	validates :qtd, numericality: {greater_than_or_equal_to: 1}
+	validates :valor, numericality: {greater_than_or_equal_to: 0}
+	validates :nome, uniqueness: true
+	validates :nome, length: {maximum: 50}
+
 end
