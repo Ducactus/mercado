a86a1c55bdc3198c86782bdff2c4f7cee3587e73
DashCell <lucas_vinicius10@live.com>
2021-03-13 20:18:12 -0300

    feat(Database): Implementa o script seed
    Inicializa o banco de dados com: "rails db:seed";
    Modifica o limite minino nescessario para uma senha de usuário.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pnomes = {0 => "Arroz",1 => "Feijão", 2 => "Manteiga", 3 => "Presunto",
+          :Arroz => 2, :Feijão => 3, :Manteiga => 2, :Presunto => 3.5}
+
+4.times do |i|
+
+    Produto.create(
+        nome: "#{pnomes[i]}",
+        qtd: 10,
+        valor: pnomes[pnomes[i].to_sym]
+    )
+
+end
+
+cnomes = {0 => "Pedro", 1 => "Jão", 2 => "Tiago", 3 => "Maria"}
+
+4.times do |i|
+    Cliente.create(
+        nome: "#{cnomes[i]}",
+        email: "example@example.com",
+        telefone: "55 5555-5555",
+        endereco: "Rua #{i+1} de setembro."
+    )
+
+end
+
+user = User.new(:email => 'root@root.com', :password => 'pass', :password_confirmation => 'pass')
+user.save
